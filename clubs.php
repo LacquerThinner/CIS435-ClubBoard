3511b45085f263fff437ea5a1f81a3f3aa8ab7cc
Kyle Driver <driver8684@gmail.com>
2022-11-27 21:44:33 -0500

    moved the site header to its own file

--- clubs.php
+++ clubs.php
@@ -10,17 +10,10 @@ else {
 }
 ?>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.101.0">
-    <title>Wolverines Unite!</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/?">
-
-<link href="assets/dist/css/bootstrap.min.css?v=1" rel="stylesheet">
+  
+  <?php
+  include('header.php');
+  ?>
 
     <style>
 		body, html {
@@ -118,11 +111,6 @@ else {
       }
     </style>
 
-    
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
-  </head>
-  <body>
   
   <?php
 	$conn = mysqli_connect('localhost', 'root', '');  
@@ -181,31 +169,7 @@ else {
     $result = mysqli_query($conn, $getQuery);   
 ?>	
     
-<header>
-     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Wolverines Unite!</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="clubs.php">Clubs</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="survey.html">Survey</a>
-          </li>
-        </ul>
-		
-		<a class="nav-link profile" href="profile.php"><img <img src=<?php echo "images/" . $photo; ?>  width="8%" height="8%" style="border-radius:50%;height:40px;width:40px;"></a>
-      </div>
-    </div>
-  </nav>
-</header>
+
 
 <?php
 if (isset($_SESSION['loggedin'])) {
