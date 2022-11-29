218a1e30bed35d738e27fdad730a7a9af20fbc1b
Kyle Driver <driver8684@gmail.com>
2022-11-29 15:42:08 -0500

    hopefully fixed profile.php

--- profile.php
+++ profile.php
@@ -15,18 +15,9 @@ else {
 }
 ?>
 <html lang="en">
-  <head>
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="social media template by w3.css">
-    <meta name="author" content="w3schools">
-    <meta name="generator" content="">
-    <title>Wolverines Unite!</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
-	
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+ <?php
+  include('header.php');
+  ?>
 	
     <style>
 	button {
@@ -96,9 +87,6 @@ else {
 	
 	<!-- Custom styles for this template -->
     <link href="carousel.css" rel="stylesheet">
-<?php
-  include('header.php');
-  ?>
 
 <?php
 $DATABASE_HOST = 'localhost';
