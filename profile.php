e1448c926aa5a3a670c098b10ae9f3a53189e481
Kyle Driver <driver8684@gmail.com>
2022-11-29 12:16:04 -0500

    testing profile.php

--- profile.php
+++ profile.php
@@ -115,9 +115,9 @@ else {
 </header>
 
 <?php
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_HOST = '141.215.80.154';
+$DATABASE_USER = 'group6';
+$DATABASE_PASS = 'DDFLa@mq7SR';
 $DATABASE_NAME = 'group6_db';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
